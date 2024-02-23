edfa150b01c755c92a7c38d609b70257c03470bb
Hkamirad <hamidrezakamranrad@yahoo.com>
2024-02-23 17:07:20 +0330

    trim and snake!

--- snakeCase.js
+++ snakeCase.js
@@ -1,6 +1,10 @@
-let str = "Hi This is Coding Front"
+
+//it Trims & Snake_case at the same time
+
+let str = "          Hi This is Coding Front            "
 let result = ""
 for (let q = 0; q < str.length; q++){
-    if (str[q] === " ") {result += "_"} else { result += str[q]} 
+    if (str[q] !== " ") {result += str[q]}
+   else if (str[q + 1] !== " " && str[q - 1] !== " ") {result += "_"}
 }
 console.log(result)
