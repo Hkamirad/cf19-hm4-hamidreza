340bccf41a543a7ee4920548429858043ba8f998
Hkamirad <hamidrezakamranrad@yahoo.com>
2024-02-23 19:05:10 +0330

    reverse anything!

--- Reverse.js
+++ Reverse.js
@@ -6,20 +6,4 @@ randomNumbers[i] = randomNumbers[j - i]
 randomNumbers[j - i] = holder
 }
 
-
 console.log(randomNumbers)
- 
-
-//OR
-
-
-
-const randomNumberss = [53, 16, 65, 100, 64, 83, 41, 98, 16, 93];
-for(i = randomNumberss.length - 1; i >= 0 ; i--){
-randomNumberss.push(randomNumberss[i])
-}
-for(i = 0; i< randomNumberss.length - 1 ; i++){
-    randomNumberss.shift()
-}
-console.log(randomNumberss)
- 
