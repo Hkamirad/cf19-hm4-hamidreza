7c70880b5fff6f234bf5759dc5582af30f577874
Hkamirad <hamidrezakamranrad@yahoo.com>
2024-03-08 19:01:40 +0330

    making it better

--- Daily/Multiplication_table.js
+++ Daily/Multiplication_table.js
@@ -1,6 +1,7 @@
-let result = ''
-for(i = 1; i < 10; i++){
-    for(j = 1; j < 11; j++){result += i + '*' + j + ':'+ i * j + '   '
-    }
+let result = [];
+for (i = 1; i < 10; i++) {
+  for (j = 1; j < 11; j++) {
+    result.push(i + " * " + j + " = " + i * j);
+  }
 }
-console.log(result)
+console.log(result);
